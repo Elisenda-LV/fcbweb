5820504f3e493b4c95c5339462fddcf1855fc96e
ElisendaLopez <elisenda.lv@gmail.com>
2024-06-15 17:06:24 +0200

    core structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component'
 import { HttpClientModule, HttpClient } from '@angular/common/http'
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { CoreModule } from './core/core.module'
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -23,6 +24,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         deps: [HttpClient],
       },
     }),
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
